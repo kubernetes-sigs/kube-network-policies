1543ba0e35c3b1efc6a4be7e72a988c1288f51cc
Antonio Ojea <aojea@google.com>
2025-08-11 22:47:32 +0000

    add api.PodInfo that contains the information to match on network policies

---
+++ hack/generate-proto.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+REPO_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+
+cd $REPO_ROOT
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+protoc --go_out=paths=source_relative:. --go-grpc_out=paths=source_relative:. pkg/api/api.proto
